f2477731148b431e266ee7a6aaee58098f8802db
Cory Quirk <coryjquirk@gmail.com>
2020-11-24 22:22:28 -0600

    improved gif

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require("express");
 
 var PORT = process.env.PORT || 8080;
-
 var app = express();
+var compression = require('compression')
+app.use(compression({ filter: shouldCompress }))
+ 
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
